abd9d3d48c15472056dcf1e7b76a7d2a1b9da33b
daniil-loban <dungeon_lda@mail.ru>
2024-04-23 20:25:12 +0300

    add a base functionality

--- src/layouts/main.py
+++ src/layouts/main.py
@@ -2,34 +2,53 @@ from sys import argv, exit
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
+from ..managers.textManager import TextManager
 
 class Window(QMainWindow):
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
+        self.text_manager = TextManager()
         self.setWindowTitle('Speedykeys')
         self.centralwidget = QWidget()
         self.setCentralWidget(self.centralwidget)
         self.layout = QGridLayout(self.centralwidget)
-        self.textedit = QTextEdit(self.centralwidget) 
+        self.textedit = QTextEdit(self.centralwidget)
+        self.setup_textedit()
+        self.layout.addWidget(self.textedit, 1, 1)
+        self.move_cursor(0)
+        self.textedit.setFocus()
+        self.textedit.setText(self.text_manager.get_formatted_text())
+    
+    def setup_textedit(self):
+        self.textedit.textChanged.connect(self.changed)
         self.textedit.setObjectName(u"textedit")
+        self.textedit.setWordWrapMode(QTextOption.WrapMode.WordWrap)
+        self.stylize_textedit()
+
+    def stylize_textedit(self):
+        self.textedit.setCursorWidth(3)
         self.textedit.setStyleSheet('''
             #textedit {
+                caret-color: white;                    
                 background-color: #FF7F50;
                 color: #0000CD;
                 font: 16pt "Lato";
             }
         ''')
 
-        self.textedit.setWordWrapMode(QTextOption.WrapMode.WordWrap)
-        self.layout.addWidget(self.textedit, 1, 1)
-        self.textedit.setFocus()
+    def changed(self):
+        textA = self.textedit.toPlainText()
+        textB = self.text_manager.get_text()
+        cur_pos = self.text_manager.cursor_pos
+        if len(textA) != len(textB):
+            self.textedit.setText(self.text_manager.get_formatted_text())
+            if cur_pos < len(textB) and textA[cur_pos] == textB[cur_pos]:
+                self.text_manager.increment_pos()
+                self.move_cursor(cur_pos + 1)
+            else:
+                self.move_cursor(cur_pos)
 
-        text = 'Все началось со статьи Snow Fall. The Avalanche at Tunnel Creek, опубликованной в 2012 году журналом The New York Times. Интерактивная история о нескольких горнолыжниках, погребенных под снегом из-за схода лавины в горах США, мгновенно набрала невиданную популярность. Через неделю после публикации главный редактор The New York Times Джилл Абрамсон отчитывалась о 2,9 млн. визитов, и 3,5 млн. просмотрах страниц. На пике 22,000 пользователей читали статью единовременно. На прочтение читатели тратили примерно 12 минут, часто дольше. В 2020 статья не выглядит ошеломляющей, за это время появилось много проектов: более сложных технически, более разнообразных в подаче. Первыми в публикации мультимедийного материала The New York Times тоже не были. Почему Snow Fall? Несмотря на технологические фокусы, ключевым в истории был качественный текст. Обогащенный дополнительными материалами — фото, видео, комментариями текст превратился в отличный контент. К нему добавили 3D модель гор и завывания вьюги — получили очень эмоциональный эффект присутствия.'
-        index = 50
-        before = text[0:index]
-        after = text[index:]
-        self.textedit.append(f"""<span style='color:yellow'>{before}</span>{after}""")
-
+    def move_cursor(self, pos=0):
         cursor = self.textedit.textCursor()
-        cursor.movePosition(QTextCursor.Start)
+        cursor.setPosition(pos)
         self.textedit.setTextCursor(cursor)
